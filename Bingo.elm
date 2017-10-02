76dd19ef1a7348b550ff16e57d6fb4536e542aab
CJ Moore <mooc123@gmail.com>
2017-10-02 11:48:46 -0600

    lesson 11

--- Bingo.elm
+++ Bingo.elm
@@ -3,10 +3,43 @@ module Bingo exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+-- Model
+type alias Entry =
+        { id : Int
+        , phrase : String
+        , points : Int
+        , marked : Bool }
 
+type alias Model = 
+         { name : String
+         , gameNumber: Int
+         , entries : List Entry  }
+
+
+initialModel : Model
+initialModel = 
+        { name = "Mike",
+          gameNumber = 1,
+          entries = initialEntries 
+        }
+
+
+initialEntries : List Entry 
+initialEntries = 
+        [ Entry 1 "Future-Proof" 100 False 
+        , Entry 2 "Doing Agile" 200 False
+        , Entry 3 "In The Cloud" 300 False
+        , Entry 4 "Rock-Star Ninja" 400 False
+        ]
+
+-- View
+
+
+playerInfo : String -> Int -> String
 playerInfo name gameNumber = 
         name ++ " - Game #" ++ (toString gameNumber)
 
+viewPlayer : String -> Int -> Html msg
 viewPlayer name gameNumber = 
         let
          playerInfoText = 
@@ -17,24 +50,45 @@ viewPlayer name gameNumber =
                 h2 [ id "info" , class "classy" ] 
                    [ playerInfoText ]
 
+viewHeader : String -> Html msg
 viewHeader title = 
         header []
                 [ h1 [] [ text title ] ]
 
+viewFooter : Html msg
 viewFooter = 
-        footer []
+        footer [ ]
                 [ a [ href "http://elm-lang.org" ]
                     [ text "Powered By Elm" ] 
                 ]
 
-view = 
+viewEntryList : List Entry -> Html msg
+viewEntryList entries = 
+        let
+            listOfEntries =
+                   List.map viewEntryItem entries 
+        in
+            ul [] listOfEntries 
+           
+           
+viewEntryItem : Entry -> Html msg
+viewEntryItem entry =
+        li [  ]
+           [ span [ class "phrase" ] [text entry.phrase] 
+           , span [ class "points" ] [text (toString entry.points) ]]
+
+view : Model -> Html msg
+view model = 
         div [class "content"]
         [viewHeader "BUZZWORD BINGO",
-         viewPlayer "Cj" 4,
+         viewPlayer model.name model.gameNumber,
+         viewEntryList model.entries,
+         div [class "debug"] [ text (toString model) ],
          viewFooter
          ]
 
+main : Html msg
 main = 
-       view
+       view initialModel
 
         
